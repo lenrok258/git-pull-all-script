6f1cb16c10d24c472e2c1cfd7dbac4ffc2267410
kornel.zemla <kornel.zemla@ocado.com>
2016-11-24 15:06:25 +0100

    Executing on-git-pull.sh if present

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -2,28 +2,36 @@
 
 cd $(dirname $0)
 
-# ############################################################
-# Functions:
+# Log file
+echo "" > git-pull-all.log;
 
-function myEcho {
+function log {
 	textToEcho=$1;
 	echo $textToEcho | tee -a git-pull-all.log
 }
 
-
-# ############################################################
-# Main:
-
-echo "" > git-pull-all.log;
-
+# Iterate over all folders in current location 
 for i in `ls -d */`; do 
-	myEcho "Start: $i";
+	log "Start: $i";
 	pushd $i;
-	git pull  2>&1 | tee -a ../git-pull-all.log;
-	popd;
-	myEcho "End: $i";
-	myEcho "----------------------------------------------------";
-	myEcho;
+
+    # If there are any changes in remote repo - make a pull
+    anyChanges=$(git fetch --dry-run 2>&1)
+    if [ ! -z "${anyChanges}" ]; then
+        log "New changes found. About to pull $i";
+        git pull 2>&1 | tee -a ../git-pull-all.log;
+
+        # If on-git-pull.sh is present - execute it
+        if [[ -x "on-git-pull.sh" ]]; then
+            log "Script 'on-git-pull.sh' found in $i. About to execute it.";
+            source ./on-git-pull.sh
+        fi;
+    fi;    
+	
+    popd;
+	log "End: $i";
+	log "----------------------------------------------------";
+	log;
 done
 
 echo;
