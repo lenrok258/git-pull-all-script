e82c2b36555174d5d93eae29304108b27c27e15d
Kornel Zemla <kornelzemla@gmail.com>
2016-11-26 16:54:35 +0100

    Fixing paths

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+source $HOME/.profile
 cd $(dirname $0)
 
-# Log file
-echo "" > git-pull-all.log;
+SCRIPT_DIR=$(pwd)
+LOG_FILE_FULL_PATH=${SCRIPT_DIR}/git-pull-all.log
+
+
+# Clean up/create log file
+echo "" > ${LOG_FILE_FULL_PATH}
 
 function log {
 	textToEcho=$1;
-	echo $textToEcho | tee -a git-pull-all.log
+	echo $textToEcho | tee -a ${LOG_FILE_FULL_PATH}
 }
 
-# Iterate over all folders in current location 
-for i in `ls -d */`; do 
+# Iterate over all folders in current location
+for i in `ls -d */`; do
 	log "Start: $i";
 	pushd $i;
 
@@ -19,15 +24,15 @@ for i in `ls -d */`; do
     anyChanges=$(git fetch --dry-run 2>&1)
     if [ ! -z "${anyChanges}" ]; then
         log "New changes found. About to pull $i";
-        git pull 2>&1 | tee -a ../git-pull-all.log;
+        git pull 2>&1 | tee -a ${LOG_FILE_FULL_PATH}
 
         # If on-git-pull.sh is present - execute it
         if [[ -x "on-git-pull.sh" ]]; then
             log "Script 'on-git-pull.sh' found in $i. About to execute it.";
-            source ./on-git-pull.sh
+            ./on-git-pull.sh | tee -a ${LOG_FILE_FULL_PATH}
         fi;
-    fi;    
-	
+    fi;
+
     popd;
 	log "End: $i";
 	log "----------------------------------------------------";
